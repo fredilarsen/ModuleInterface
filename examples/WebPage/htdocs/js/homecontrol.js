9a449e1acfb6379661335833fbe12a5a419e414f
fredilarsen <fredilarsen@hotmail.com>
2017-12-31 16:55:02 +0100

    Improved auto-updating of web pages
    Added test to retrieval of outputs and settings into web page so that a
    new retrieval will not be started if the previous is not completed.

--- examples/WebPage/htdocs/js/homecontrol.js
+++ examples/WebPage/htdocs/js/homecontrol.js
@@ -1,83 +1,104 @@
-// This function sends modified values back to the server	
+// This function sends modified values back to the server  
 $(function(){
     //var message_status = $("#statusText");
     $("td[contenteditable=true]").blur(function(){
         var field_id = $(this).attr("id");
         var value = $(this).text() ;
-		console.log("Storing to db " + field_id + " value " + value);
-		$(this).removeClass("userEditing");
+    console.log("Storing to db " + field_id + " value " + value);
+    $(this).removeClass("userEditing");
         $.post('set_setting.php' , field_id + "=" + value, function(data){
 
 //            if(data != '')
-//			{
-//				message_status.show();
-//				message_status.text("Setting " + field_id + ", " + data);
-				//hide the message
-//				setTimeout(function(){message_status.hide()},15000);
-//			}
+//      {
+//        message_status.show();
+//        message_status.text("Setting " + field_id + ", " + data);
+        //hide the message
+//        setTimeout(function(){message_status.hide()},15000);
+//      }
         });
     });
 });
 
-// This function marks editable fields when focused	
+// This function marks editable fields when focused  
 $(function(){
     $("td[contenteditable=true]").focus(function(){
-		$(this).addClass("userEditing");
+    $(this).addClass("userEditing");
     });
 });
 
 // This function fills values from the database into the fields
 $( document ).ready(function() {
-	updateTablesWithMeasurements();
+  updateTablesWithMeasurements();
     updateTablesWithSettings();
-	setInterval(updateTablesWithMeasurements, 5000);
-	setInterval(updateTablesWithSettings, 5000);
+  setInterval(updateTablesWithMeasurements, 5000);
+  setInterval(updateTablesWithSettings, 5000);
   });
   
+  var startedUpdating = 0;
+  var startedUpdatingSettings = 0;
+  
   function updateTablesWithMeasurements() {
+    // Avoid updating if last attempt is not complete
+    var n = new Date().getMilliseconds();
+    if (startedUpdating > 0) {
+      if (n - startedUpdating < 60000) {
+        console.log("Outputs already updating!");
+        return;
+      }
+      else console.log("Outputs already updating, but has timed out!");
+    }
+    startedUpdating = n;
+
     var table_meas = $( "#hhMeasurements" );
     var table_outputs = $( "#hhOutputs" );
     var table_inputs = $( "#hhInputs" );
-    $.getJSON("get_currentvalues.php", "hh", function ( data ) {
+    $.getJSON("get_currentvalues.php", function ( data ) {
       $.each( data, function( i, item ) {
-		if (table_meas != null) {
-	      var cell = table_meas.find('[id="' + i + '"]');
-	      if (cell.length > 0) {
-//		  console.log("Meas i=" + i + ", item=" + item);
-		    cell.text(item);
-	      }
-	    }
-	    if (table_outputs != null) {
-	      var cell = table_outputs.find('[id="' + i + '"]');
-	      if (cell.length > 0) {
-//		  console.log("Output i=" + i + ", item=" + item);
-		    cell.text(item);
-	      }
-	    }
-	    if (table_inputs != null) {
-	      var cell = table_inputs.find('[id="' + i + '"]');
-	      if (cell.length > 0) {
-//		  console.log("Input i=" + i + ", item=" + item);
-		    cell.text(item);
-	      }
-	    }      
-	  });
-	});	  
+        if (table_meas.length) {
+          var cell = table_meas.find('[id="' + i + '"]');
+          if (cell.length > 0) cell.text(item);
+        }
+        if (table_outputs.length) {
+          var cell = table_outputs.find('[id="' + i + '"]');
+          if (cell.length > 0) cell.text(item);
+        }
+        if (table_inputs.length) {
+          var cell = table_inputs.find('[id="' + i + '"]');
+          if (cell.length > 0) cell.text(item);
+        }      
+      });
+    })
+    .always(function() {
+      startedUpdating = 0;    
+    });
   }
   
   function updateTablesWithSettings() {
-	var table_settings = $( "#hhSettings" );
-    if (table_settings != null) {
-	  $.getJSON("get_settings.php", "hh", function ( data ) {
-		$.each( data, function( i, item ) {
-			var cell = table_settings.find('[id="' + i + '"]');
-			if (cell.length > 0 && !cell.hasClass("userEditing")) { // Do not change if user is editing
-	//		    console.log("Setting i=" + i + ", item=" + item);
-			  cell.text(item);
-			  postValueChangeAction(i, cell);
-			}
-		});
-	  });
+    var table_settings = $( "#hhSettings" );
+    if (table_settings.length) {
+      // Avoid updating if last attempt is not complete
+      var n = new Date().getMilliseconds();
+      if (startedUpdatingSettings > 0) {
+        if (n - startedUpdatingSettings < 60000) {
+          console.log("Settings already updating!");
+          return;
+        } 
+        else console.log("Settings already updating, but has timed out!");
+      }
+      startedUpdatingSettings = n;
+        
+      $.getJSON("get_settings.php", function ( data ) {
+        $.each( data, function( i, item ) {
+          var cell = table_settings.find('[id="' + i + '"]');
+          if (cell.length > 0 && !cell.hasClass("userEditing")) { // Do not change if user is editing
+            cell.text(item);
+            postValueChangeAction(i, cell);
+          }
+        });
+      })
+      .always(function() {
+        startedUpdatingSettings = 0;    
+      });
     }
   }
   
@@ -90,16 +111,16 @@ $( document ).ready(function() {
   }
   
   function getMinutesOfDayAsInfo(minutesOfDay) {
-	if (minutesOfDay == null || minutesOfDay == "") return "";
-	var hours = Math.floor(minutesOfDay / 60),
-	    minutes = Math.floor(minutesOfDay - hours*60);
-	return (hours <= 9 ? "0" + hours : "" + hours) + ":" 
-	  + (minutes <= 9 ? "0" + minutes : "" + minutes);	
+  if (minutesOfDay == null || minutesOfDay == "") return "";
+  var hours = Math.floor(minutesOfDay / 60),
+      minutes = Math.floor(minutesOfDay - hours*60);
+  return (hours <= 9 ? "0" + hours : "" + hours) + ":" 
+    + (minutes <= 9 ? "0" + minutes : "" + minutes);  
   }
 
   function getUTCAsTimeInfo(UTC) {
-	if (new Date(1000*UTC) < new Date()) return "Off";
-	return "+" + Math.round((new Date(1000*UTC).getTime() - new Date().getTime())/60000) + " min"; //toISOString().slice(0,-5).replace('T',' ');
+  if (new Date(1000*UTC) < new Date()) return "Off";
+  return "+" + Math.round((new Date(1000*UTC).getTime() - new Date().getTime())/60000) + " min"; //toISOString().slice(0,-5).replace('T',' ');
   }
 
   function getQueryVariable(variable)
