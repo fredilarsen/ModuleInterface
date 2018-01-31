b51b327cc5028a4618dbbbe9c1978c4ccbc535c5
fredilarsen <fredilarsen@hotmail.com>
2018-01-31 21:33:00 +0100

    Changed header file names

---
+++ src/MIModule.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <PJON.h>
+#include <platforms/platforms.h>
+
+#include <MI_PJON/PJONModuleInterface.h>
+#include <MI_PJON/PJONLink.h>
+#include <MI/ModuleInterfacePersistence.h>
+#include <utils/MIUtilities.h>
