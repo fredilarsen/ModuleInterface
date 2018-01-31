66018d2727810f228ecd1e6434e7cb17c2af0d60
fredilarsen <fredilarsen@hotmail.com>
2018-01-31 21:32:29 +0100

    Deleted files to change a character to uppercase

--- src/MiModule.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <PJON.h>
-#include <platforms/platforms.h>
-
-#include <MI_PJON/PJONModuleInterface.h>
-#include <MI_PJON/PJONLink.h>
-#include <MI/ModuleInterfacePersistence.h>
-#include <utils/MIUtilities.h>
