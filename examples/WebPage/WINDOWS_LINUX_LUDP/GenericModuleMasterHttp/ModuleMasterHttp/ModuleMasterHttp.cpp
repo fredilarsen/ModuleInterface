952ce1b750c0887e2b39aa08af64c8cd51999e7f
fredilarsen <fredilarsen@hotmail.com>
2018-03-16 22:50:49 +0100

    Changed order of command line parameters, make prefix optional

--- examples/WebPage/WINDOWS_LINUX_LUDP/GenericModuleMasterHttp/ModuleMasterHttp/ModuleMasterHttp.cpp
+++ examples/WebPage/WINDOWS_LINUX_LUDP/GenericModuleMasterHttp/ModuleMasterHttp/ModuleMasterHttp.cpp
@@ -32,23 +32,26 @@ void parse_ip_string(const char *ip_string, in_addr &ip) {
 void setup(int argc, const char * const argv[]) {
   printf("Welcome to ModuleMasterHttp.\n");
 
-  if (argc < 3) {
-    printf("ERROR: Master prefix (e.g. m1) and IP address of web server must be specified on the command line.\n");
+  if (argc < 2) {
+    printf("ERROR: The IP address of web server must be specified on the command line.\n");
     exit(2);
   }
 
-  // Get the master prefix
-  String master_prefix = argv[1];
-  if (master_prefix.length() != 2) {
-    printf("ERROR: Master prefix '%s' must have length 2.\n", master_prefix.c_str());
-    exit(3);
-  }
-
   // Parse web server IP address specified on command line
-  String webserver = argv[2];
+  String webserver = argv[1];
   in_addr web_server_ip;
   parse_ip_string(webserver.c_str(), web_server_ip);
   printf("Using web server at %s.\n", webserver.c_str());
+
+  // Get the master prefix
+  String master_prefix = "m1";
+  if (argc > 2) {
+    master_prefix = argv[1];
+    if (master_prefix.length() != 2) {
+      printf("ERROR: Master prefix '%s' must have length 2.\n", master_prefix.c_str());
+      exit(3);
+    }
+  }
 
   // Now get the rest of the configuration from the web server
   bus.bus.strategy.set_port(7200); // Use the same port on all devices
