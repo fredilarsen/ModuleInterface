765704dff208e6a4849423c33d0f858afb57bf4c
fredilarsen <fredilarsen@hotmail.com>
2018-06-24 18:33:27 +0200

    Support using system time instead of own time on Linux/Windows
    Instead of keeping its own time, retrieved from the web server, system time can be used directly on platforms with a realtime clock, like Windows and Linux. By defining MI_USE_SYSTEMTIME the time(0) library call will always be used when getting time.

--- src/utils/MITime.cpp
+++ src/utils/MITime.cpp
@@ -9,25 +9,56 @@ uint32_t miTimeS = 0,                 // current time in UTC seconds, maintained
          miLastSyncedMs = 0;          // system time in ms() for last setTime() call from external time source
  
 void miUpdateTime() {
+#ifndef MI_USE_SYSTEMTIME
   // Update current time
   uint32_t msElapsed = (uint32_t)(millis() - miLastUpdatedTimeMs), sElapsed = msElapsed/1000ul;
-    miTimeS += sElapsed;
-    miLastUpdatedTimeMs += sElapsed*1000ul;
-  }
+  miTimeS += sElapsed;
+  miLastUpdatedTimeMs += sElapsed*1000ul;
+#endif  
+}
 
 void miSetTime(uint32_t utc_seconds) {
+#ifndef MI_USE_SYSTEMTIME
   miUpdateTime();
   if (MI_SECONDS_YEAR_2017 < utc_seconds) { // Do not accept invalid time
     miTimeS = utc_seconds;
     miLastUpdatedTimeMs = miLastSyncedMs = millis();
+  }
+#endif  
 }
+
+uint32_t miGetTime() {
+#ifdef MI_USE_SYSTEMTIME
+  // Do not keep track of time, just use the system time
+  return time(0);
+#else
+  miUpdateTime(); return miTimeS;
+#endif
 }
-  
-uint32_t miGetTime() { miUpdateTime(); return miTimeS; }
-  
-bool miIsTimeSynced() { miUpdateTime(); return miLastSyncedMs != 0 && MI_SECONDS_YEAR_2017 < miTimeS; }
-bool miWasSyncedWithin(uint32_t limit_ms) { miUpdateTime(); return ((uint32_t)(millis() - miLastSyncedMs) <= limit_ms); }
-uint32_t miGetLastSyncedAtMs() { miUpdateTime(); return miLastSyncedMs; }
+
+bool miIsTimeSynced() {
+#ifdef MI_USE_SYSTEMTIME
+  return MI_SECONDS_YEAR_2017 < time(0):
+#else
+  miUpdateTime(); return miLastSyncedMs != 0 && MI_SECONDS_YEAR_2017 < miTimeS;
+#endif
+}
+
+bool miWasSyncedWithin(uint32_t limit_ms) {
+#ifdef MI_USE_SYSTEMTIME
+  return miIsTimeSynced();
+#else
+  miUpdateTime(); return ((uint32_t)(millis() - miLastSyncedMs) <= limit_ms);
+#endif  
+}
+
+uint32_t miGetLastSyncedAtMs() { 
+#ifdef MI_USE_SYSTEMTIME
+  return miIsTimeSynced() ? miGetTime() : 0;
+#else
+  miUpdateTime(); return miLastSyncedMs;
+#endif  
+}
   
 uint8_t miGetWeekDay(uint32_t utc_s) { return ((utc_s / MI_SECONDS_PER_DAY + 3) % 7ul); } // 0=monday
 uint32_t miGetSecondOfDay(uint32_t utc_s) { return utc_s % MI_SECONDS_PER_DAY; }
