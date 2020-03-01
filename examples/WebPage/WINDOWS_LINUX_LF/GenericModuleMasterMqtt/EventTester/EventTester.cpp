00cdd1ee4cd45bfc0de8ae77e88a99dbfee31b9b
Fred Larsen <fredilarsen@hotmail.com>
2020-03-01 18:53:00 +0100

    Added master+module example based on only MQTT, using LF strategy

---
+++ examples/WebPage/WINDOWS_LINUX_LF/GenericModuleMasterMqtt/EventTester/EventTester.cpp
@@ -0,0 +1,119 @@
+/* This is a test program for using events to and from a MQTT broker.
+   An event is an extraordinary prioritized transfer of one or more values,
+   flagged to be transferred earlier than the first scheduled transfer.
+*/
+
+#define PJON_INCLUDE_LF
+#define MI_USE_SYSTEMTIME
+#define DEBUG_PRINT_TIMES
+//#define DEBUG_PRINT_SETTINGSYNC
+
+#ifdef _WIN32
+  // MS compiler does not like PJON_MAX_PACKETS=0 in PJON
+  #define PJON_MAX_PACKETS 1
+#endif
+
+#include <MIModule.h>
+#include <utils/MITime.h>
+#include <utils/MIUtilities.h>
+
+// PJON related
+PJONLink<LocalFile> bus(20); // PJON device id 20
+
+PJONModuleInterface interface("LightCon", // Module name
+  bus,                                    // PJON bus
+  "Target:f4",                            // Settings
+  "ETemp:f4",                             // Inputs                       
+  "Temp:f4 TTarg:f4 UTC:u4");             // Outputs (measurements)                         
+
+// Settings
+#define s_target_ix       0
+
+// Inputs
+#define i_externaltemp_ix 0
+
+// Outputs
+#define o_temp_ix         0
+#define o_target_ix       1
+#define o_utc_ix          2
+
+uint32_t last_out_event_time = 0, last_setting_event_time = 0, last_control_time = 0;
+
+float target = 20, temp = 20;
+
+
+void lightcontrol() {
+  // Require that all settings, inputs and time sync are updated before starting
+  if (interface.settings.is_updated() && interface.is_time_set()) {
+/*
+    if (mi_interval_elapsed(last_out_event_time, 8000)) {
+      temp += 3.0;
+      interface.outputs.set_value(o_temp_ix, temp);
+      interface.outputs.set_event(o_temp_ix); // Flag it for immediate transfer
+      printf("Increase temp output to %g\n", temp);
+    }
+*/
+/*
+    // Set target every now and then from the device
+    if (mi_interval_elapsed(last_setting_event_time, 17000)) {
+      target = 20;
+      interface.settings.set_value(s_target_ix, target);
+      interface.settings.set_event(s_target_ix); // Flag it for immediate transfer
+      printf("Set target setting to %g\n", target);
+    }
+*/
+    // Step controller towards target temp every second
+    if (mi_interval_elapsed(last_control_time, 1000)) { 
+      temp += (target - temp) / 50.0; 
+      interface.outputs.set_value(o_temp_ix, temp);
+      interface.outputs.set_event(o_temp_ix);
+    }
+  }
+}
+
+void notification_function(NotificationType notification_type, const ModuleInterface *) {
+  if (notification_type == ntSampleOutputs) {
+    printf("Sampling outputs\n");
+    interface.outputs.set_value(o_temp_ix, temp);
+    interface.outputs.set_value(o_target_ix, target);
+    interface.outputs.set_value(o_utc_ix, interface.get_time_utc_s());
+    interface.outputs.set_updated();
+  }
+  else if (notification_type == ntNewInputs) {
+    // Show an input value
+    float exttemp = 0;
+    interface.inputs.get_value(i_externaltemp_ix, exttemp);
+    if (interface.inputs.get_module_variable(i_externaltemp_ix).is_event())
+      printf("Incoming input EVENT, exttemp is %f\n", exttemp); 
+    else
+      printf("Incoming timer-based input, exttemp is %f\n", exttemp);
+  }
+  else if (notification_type == ntNewSettings) {
+    // New settings
+    target = interface.settings.get_float(s_target_ix);
+    if (interface.settings.get_module_variable(s_target_ix).is_event()) {
+      printf("Incoming setting EVENT, target = %g\n", target);
+    } else
+      printf("Incoming timer-based setting, target = %g\n", target);
+  }
+}
+
+void setup(int argc, const char * const argv[]) {
+  printf("Welcome to EventTester (LocalFile).\n");
+
+  // Now get the rest of the configuration from the web server
+  bus.bus.begin();
+  interface.set_notification_callback(notification_function);
+}
+
+void loop() {
+  interface.update();
+  lightcontrol();
+  delay(1);
+}
+
+int main(int argc, const char * const argv[]) {
+  setup(argc, argv);
+  while (true) loop();
+  return 0;
+}
---
+++ examples/WebPage/WINDOWS_LINUX_LF/GenericModuleMasterMqtt/GenericModuleMasterMqtt/GenericModuleMasterMqtt.cpp
@@ -0,0 +1,106 @@
+/* This example demonstrates how to connect your ModuleInterface based setup to
+*  a MQTT broker. Settings, outputs and inputs are transferred through a MQTT broker 
+*  for easy connection to/from systems like Home Assistant and OpenHAB.
+*
+* This example uses _only_ a connection to a MQTT broker, and no connection to the ModuleInterface
+* web pages or databases. See the other master example that is connected to both systens at once.
+* This example also uses the PJON LocalFile strategy to make it easy to run master and modules as 
+* processes on the same computer, for test purposes. No extra hardware needed.
+* Notice how the list of modules is hardcoded in this example while it is retrieved dynamically 
+* from the database in the generic master example.
+*/
+
+#define PJON_INCLUDE_LF
+#define MI_USE_SYSTEMTIME
+#define DEBUG_PRINT_TIMES
+#define MI_USE_MQTT
+#define MASTER_MULTI_TRANSFER
+//#define DEBUG_PRINT_SETTINGSYNC
+//#define MQTT_DEBUGPRINT
+//#define MIMQTT_USE_JSON
+
+#ifdef _WIN32
+  // MS compiler does not like PJON_MAX_PACKETS=0 in PJON
+  #define PJON_MAX_PACKETS 1
+#endif
+
+#include <MIMaster.h>
+#include <MI/ModuleInterfaceMqttTransfer.h>
+
+// PJON related
+PJONLink<LocalFile> bus(1); // PJON device id 1
+
+// Module interfaces
+PJONModuleInterfaceSet interfaces(bus, (const char *)NULL);
+
+MIMqttTransfer mqtt_transfer(interfaces, NULL);
+MITransferBase *transfers[1] = { &mqtt_transfer };
+
+void parse_ip_string(const char *ip_string, in_addr &ip) {
+  if (!inet_pton(AF_INET, ip_string, &ip)) {
+    printf("ERROR: IP address '%s' in not in a valid notation.\n", ip_string);
+    exit(1);
+  }  
+}
+
+void setup(int argc, const char * const argv[]) {
+  printf("Welcome to GenericModuleMaster (LocalFile+MQTT).\n");
+
+  if (argc < 2) {
+    printf("ERROR: The IP address of a MQTT broker must be specified on the command line.\n");
+    printf("Parameters: <IPv4 address in dot notation> [<MQTT port number> [<master prefix>]]\n");
+    printf("(Default port number is 1884. Default master prefix is 'm1'.)\n");
+    exit(2);
+  }
+
+  // Parse MQTT broker IP address specified on command line
+  String ip = argv[1];
+  in_addr broker_ip;
+  parse_ip_string(ip.c_str(), broker_ip);
+  printf("Using MQTT broker at %s.\n", ip.c_str());
+
+  // Get the MQTT port number if present
+  uint16_t port_number = 1883;
+  if (argc > 2) {
+    port_number = atoi(argv[2]);
+    if (port_number == 0) {
+      printf("ERROR: MQTT broker port number '%s' must be a valid integer.\n", argv[2]);
+      exit(3);
+    }
+  }
+
+  // Get the master prefix
+  String master_prefix = "m1";
+  if (argc > 3) {
+    master_prefix = argv[3];
+    if (master_prefix.length() != 2) {
+      printf("ERROR: Master prefix '%s' must have length 2.\n", master_prefix.c_str());
+      exit(4);
+    }
+  }
+
+  bus.bus.begin();
+  interfaces.set_prefix(master_prefix.c_str());
+  interfaces.sampling_time_outputs = interfaces.sampling_time_settings = 20000;
+  interfaces.set_interface_list("EvtTest:et:20");  
+
+  // Set up MQTT connection
+  mqtt_transfer.set_broker_address((uint8_t*)&broker_ip, port_number);
+  mqtt_transfer.start();
+
+  interfaces.set_external_transfer(sizeof transfers / sizeof transfers[0], transfers);
+}
+
+void loop() {
+  interfaces.update();
+  delay(1);
+}
+
+int main(int argc, const char * const argv[]) {
+  setup(argc, argv);
+  while (true) loop();
+  return 0;
+}
+
+
+
